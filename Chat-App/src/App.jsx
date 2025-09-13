00660c81ff73cec90140dc77ded637a4eafa23e2
sunnyahuja538 <sunnyahuja538@gmail.com>
2025-09-13 20:11:10 +0530

    updated

--- Chat-App/src/App.jsx
+++ Chat-App/src/App.jsx
@@ -9,7 +9,7 @@ import {  AuthDataContext } from '../context/AuthContext.jsx'
 const App = () => {
   const {authUser}=useContext(AuthDataContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser?<HomePage/>:<Navigate to='/login'/>}/>
