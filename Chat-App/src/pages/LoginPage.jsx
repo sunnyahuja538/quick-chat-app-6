d7c2c6b3f3f4f131b9d467a427c4749d23eafab1
sunnyahuja538 <sunnyahuja538@gmail.com>
2025-09-13 20:02:39 +0530

    f

--- Chat-App/src/pages/LoginPage.jsx
+++ Chat-App/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import assets from '../assets/assets';
+import assets from '../assets/assets.js';
 import { useContext } from 'react';
 import { AuthDataContext } from '../../context/AuthContext';
 
